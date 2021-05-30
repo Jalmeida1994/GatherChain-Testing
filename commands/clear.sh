b398179c932778d83686f19fdedf251e0b746fff
Joao Almeida <jcfd.almeida@campus.fct.unl.pt>
2021-05-30 10:42:41 +0100

    admin params

--- commands/clear.sh
+++ commands/clear.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Arg 1: Virtual Machine Password configured in the ARM template (https://github.com/Jalmeida1994/GatherChain-ARM-Template)
+
 source .weburl.env
 
-curl -X POST -H "Content-Type: application/json" -d "{\"Author\":\"0000\",\"Group\":\"0000\",\"Commit\":\"0000\"}" ${WEB_URL}/clear 
+curl -X POST -H "Content-Type: application/json" -d "{\"Author\":\"${1}\",\"Group\":\"0000\",\"Commit\":\"0000\"}" ${WEB_URL}/clear 
 
 
--- commands/init.sh
+++ commands/init.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# Arg 1: Virtual Machine Password configured in the ARM template (https://github.com/Jalmeida1994/GatherChain-ARM-Template)
+
 source .weburl.env
 
 initNet () {
-    if curl --fail -X POST -H "Content-Type: application/json" -d "{\"Author\":\"0000\",\"Group\":\"0000\",\"Commit\":\"0000\"}" ${WEB_URL}/init; then
+    if curl --fail -X POST -H "Content-Type: application/json" -d "{\"Author\":\"${1}\",\"Group\":\"0000\",\"Commit\":\"0000\"}" ${WEB_URL}/init; then
         printf "Network launched!"
     else
         printf "Error launching the network."
